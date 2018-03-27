7e4ae115025c5a284e3aa3d527137442c51c5496
César Alberto Trejo <cesaratj27@gmail.com>
2018-03-27 00:49:20 -0600

    Todo subido, todo en orden

--- csv-to-json.js
+++ csv-to-json.js
@@ -6,18 +6,29 @@
 const csvFileUrl = 'https://prod-edxapp.edx-cdn.org/assets/courseware/v1/07d100219da1a726dad5eddb090fa215/asset-v1:Microsoft+DEV283x+2T2017+type@asset+block/customer-data.csv'
 const csv = require('csvtojson')
 const fs = require('fs')
-const https = require('https')
+//const https = require('https')
+const request = require('request')
+const path = require('path')
 
-let data = [] // to recive data from JSON 
+var data = [] // Array to save JSON objects
 
-csv()
-.fromStream(https.get(csvFileUrl))
-.on('csv', (csvRow) => {
+csv().fromStream(request.get(csvFileUrl))/*.on('csv', (csvRow) => {
 	// csvRow is an array 
-	data.push(csvRow)
-})
-.on('done', (error) => {
-	console.log(error)
+	//data.push(JSON.stringify(csvRow))  
+})*/.on('json', (jsonObj) => {
+	//console.log(jsonObj)
+	data.push(jsonObj)
+}).on('done', (error) => {
+	//console.log(error)
 	if (error) return process.exit(1)
-
+	console.log(data)
+	// Continue writing data on JSON file
+	fs.writeFile('customer-data.json', JSON.stringify(data), (error) => {
+		if (error) return process.exit(1)
+		console.log('Process finished')
+		process.exit(0)
+	})
+}).on('error', (error) => {
+	console.error(`Got error: ${error.message}`)
 })
+
