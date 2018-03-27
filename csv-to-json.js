098080076b2ed548dcc187a77964a10d63a983d0
César Alberto Trejo <cesaratj27@gmail.com>
2018-03-26 23:51:48 -0600

    Actualizado csv-to-json.js

--- csv-to-json.js
+++ csv-to-json.js
@@ -6,12 +6,18 @@
 const csvFileUrl = 'https://prod-edxapp.edx-cdn.org/assets/courseware/v1/07d100219da1a726dad5eddb090fa215/asset-v1:Microsoft+DEV283x+2T2017+type@asset+block/customer-data.csv'
 const csv = require('csvtojson')
 const fs = require('fs')
+const https = require('https')
+
+let data = [] // to recive data from JSON 
 
 csv()
-.fromStream(request.get(csvFileUrl))
+.fromStream(https.get(csvFileUrl))
 .on('csv', (csvRow) => {
-	// csvRow is an array
+	// csvRow is an array 
+	data.push(csvRow)
 })
 .on('done', (error) => {
+	console.log(error)
+	if (error) return process.exit(1)
 
 })
